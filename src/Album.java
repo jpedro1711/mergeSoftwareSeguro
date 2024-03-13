49234ec371e469161b02c329a10cdc7e4670f1bf
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2024-03-13 20:29:38 -0300

    feat: album base class

---
+++ src/Album.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    private String nome;
+    private String dataLancamento;
+    private List<String> musicas = new ArrayList<>();
+
+    public Album(String nome, String dataLancamento) {
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Album()
+    {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public List<String> getMusicas() {
+        return musicas;
+    }
+
+    public void addMusica(String musica) {
+        this.musicas.add(musica);
+    }
+}
